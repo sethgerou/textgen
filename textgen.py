ebd8eaa1add35fbe8ca3d239fd371616c529b625
sethgerou <seth.gerou@gmail.com>
2017-07-14 10:57:26 -0700

    modified prompt for clarity

--- textgen.py
+++ textgen.py
@@ -7,7 +7,7 @@ consonants = copy(letters)
 for letter in vowels:
     consonants = consonants.replace(letter, "")
 
-prompt = 'What letters do you want? Enter "v" for vowels. "c" for consonants, "l" for any letter (separate letters with a space): '
+prompt = 'What letters do you want? Enter "v" for vowels. "c" for consonants, any other key for any letter (separate letters with a space): '
 
 input_letters = input(prompt)
 
