da823dd856b298ba38b5447fd225bc5c677a2e6a
sethgerou <seth.gerou@gmail.com>
2017-07-14 11:18:10 -0700

    remove bad user instruction to add space between letters

--- textgen.py
+++ textgen.py
@@ -7,7 +7,7 @@ consonants = copy(letters)
 for letter in vowels:
     consonants = consonants.replace(letter, "")
 
-prompt = 'What letters do you want? Enter "v" for vowels. "c" for consonants, "l" for any letter, or another letter to use that letter. (separate letters with a space): '
+prompt = 'What letters do you want? Enter "v" for vowels. "c" for consonants, "l" for any letter, or another letter to use that letter.: '
 
 quantity = input("How many names do you want? ")
 input_letters = input(prompt)
